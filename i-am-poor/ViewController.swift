b2c616f1a4edd55769b82c38961161125d135e07
vin3y <vinaysankar7@gmail.com>
2024-01-22 20:28:55 +0530

    Initial Commit

---
+++ i-am-poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  i-am-poor
+//
+//  Created by Vinay Sankar on 22/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
